21687ad1df4dcf9139bce96cef41ed96bb79229f
ign13101 <nacho-13101@hotmail.com>
2023-08-24 22:44:49 +0200

    Fixed translation for some symbols and added redundancy translation

--- src/routes/converter/brailleChars.js
+++ src/routes/converter/brailleChars.js
@@ -155,15 +155,16 @@
         í: "\u280C",
         ó: "\u282A",
         ú: "\u283E",
+        ü: "\u2833",
         Á: "\u2828\u2837",
         É: "\u2828\u283F",
         Í: "\u2828\u280C",
         Ó: "\u2828\u282A",
         Ú: "\u2828\u283E",
+        Ü: "\u2828\u2833",
         ñ: "\u283B",
         Ñ: "\u2828\u283B",
         ç: "\u282F",
-        ü: "\u2833",
 
         // Numbers fixed
         "0": "\u283C\u281A",
@@ -179,6 +180,7 @@
 
         // Common symbols
         " ": "\u2800", // Space fixed?
+        " ": "\u2800",
         ".": "\u2804", // Period fixed
         ",": "\u2802", // Comma fixed
         ";": "\u2806", // Semicolon fixed
@@ -189,23 +191,28 @@
         "¿": "\u2822", // Question Mark fixed
         "'": "\u2820\u2826", // Apostrophe fixed
         '"': "\u2826", // Quotation Mark fixed
-        "`": "\u2804", // Apostrophe fixed
+        "‘": "\u2820\u2826",
+        "”": "\u2826",
+        "`": "\u2820\u2804", // Apostrophe fixed modified
         "-": "\u2824", // Hyphen fixed
+        "–": "\u2824",
         "_": "\u2824\u2824", // Underscore fixed
         "(": "\u2823", // Left Parenthesis fixed
         ")": "\u281C", // Right Parenthesis fixed
-        "[": "\u2837", // Left Square Bracket fixed
-        "]": "\u283E", // Right Square Bracket fixed
+        "[": "\u2810\u2837", // Left Square Bracket fixed modified
+        "]": "\u2838\u283E", // Right Square Bracket fixed modified
         "{": "\u2810\u2807", // Open Curly Braces fixed
         "}": "\u2838\u2802", // Close Curly Braces fixed
         "/": "\u2820\u2802", // Slash fixed
         "\\": "\u2810\u2804", // Backwards Slash fixed
         "@": "\u2810", // At Sign fixed
-        "#": "\u2833", // Hash Sign
+        "#": "\u283C", // Hash Sign fixed
         "*": "\u2814", // Asterisk fixed
-        "+": "\u2816", // Plus Sign fixed
+        "+": "\u282C", // Plus Sign fixed
         "=": "\u2836", // Equal Sign fixed
         "\\n": "\u000A",
+        "%": "\u2838\u2834", // Percentage sign fixed
+        "©": "\u2823\u2828\u2809\u281C", // Copyright sign fixed
         // $: "\u282F", // Dollar Sign
         // "&": "\u2826", // Ampersand
         // "%": "\u2835", // Percent Sign
--- src/routes/translator/dicts.js
+++ src/routes/translator/dicts.js
@@ -62,15 +62,16 @@ const dictionaries = {
         "\u280C": "í",
         "\u282A": "ó",
         "\u283E": "ú",
+        "\u2833": "ü",
         "\u2828\u2837": "Á",
         "\u2828\u283F": "É",
         "\u2828\u280C": "Í",
         "\u2828\u282A": "Ó",
         "\u2828\u283E": "Ú",
+        "\u2828\u2833": "Ü",
         "\u283B": "ñ",
         "\u2828\u283B": "Ñ",
         "\u282F": "ç",
-        "\u2833": "ü",
 
         // Numbers fixed
         "\u283C\u281A": "0",
@@ -94,22 +95,25 @@ const dictionaries = {
         "\u2822": "?", // Question Mark fixed
         "\u2820\u2826": "'", // Apostrophe fixed
         "\u2826": '"', // Quotation Mark fixed
+        "\u2820\u2804": "`", // Apostrophe fixed modified
         "\u2824": "-", // Hyphen fixed
         "\u2824\u2824": "_", // Underscore fixed
         "\u2823": "(", // Left Parenthesis fixed
         "\u281C": ")", // Right Parenthesis fixed
-        "\u2837": "[", // Left Square Bracket fixed
-        "\u283E": "]", // Right Square Bracket fixed
+        "\u2810\u2837": "[", // Left Square Bracket fixed modified
+        "\u2838\u283E": "]", // Right Square Bracket fixed modified
         "\u2810\u2807": "{", // Open Curly Braces fixed
         "\u2838\u2802": "}", // Close Curly Braces fixed
         "\u2820\u2802": "/", // Slash fixed
         "\u2810\u2804": "\\", // Backwards Slash fixed
         "\u2810": "@", // At Sign fixed
-        "\u2833": "#", // Hash Sign
+        "\u283C": "#", // Hash Sign
         "\u2814": "*", // Asterisk fixed
-        "\u2816": "+", // Plus Sign fixed
+        "\u282C": "+", // Plus Sign fixed
         "\u2836": "=", // Equal Sign fixed
         "\u000A": "\\n",
+        "\u2838\u2834": "%", // Percentage sign fixed
+        "\u2823\u2828\u2809\u281C": "©", // Copyright sign fixed
         "\u28FF": "□", // 8 dot full symbol for blank square
         default: "\u2800",
     },
