3f8100cefb87e25491f05ab118de9e1d329818ea
ign13101 <nacho-13101@hotmail.com>
2023-10-06 14:12:18 +0200

    Updated about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -14,18 +14,39 @@
     <div class="centered-content">
         <br />
         <br />
-        <br />
         <h2>¿Qué es EBRAI<span class="lowered-text">⠯</span>?</h2>
         <br />
         <div class="text-focus">
             EBRAI<span class="lowered-text">⠯</span> es un innovador proyecto de
-            Trabajo de Fin de Grado, una culminación académica en la etapa universitaria
-            en la que se aplican conocimientos adquiridos para crear soluciones reales.
-            Se enfoca en eliminar las barreras entre los libros electrónicos y las
-            personas con deficiencias visuales. Permite convertir libros electrónicos
-            en formato EPUB a PEF, facilitando la impresión de libros físicos en
-            Braille y la lectura de estos libros electrónicos en pantallas de Braille
-            actualizables, promoviendo así una experiencia de lectura inclusiva.
+            Trabajo de Fin de Grado, una culminación académica en la etapa
+            universitaria en la que se aplican conocimientos adquiridos para
+            crear soluciones reales. Se enfoca en eliminar las barreras entre
+            los libros electrónicos y las personas con deficiencias visuales.
+            Permite convertir libros electrónicos en formato EPUB a PEF,
+            facilitando la impresión de libros físicos en Braille y la lectura
+            de estos libros electrónicos en pantallas de Braille actualizables,
+            promoviendo así una experiencia de lectura inclusiva.
+            <br />
+            <br />
+            Además de la página de conversión, también dispone de una página de extracción
+            del texto en Braille de los archivos PEF, para poder comprobar el correcto
+            funcionamiento del conversor, y una página de traductor, donde puedes
+            introducir las filas de la página de visor para comprobar que la traducción
+            es correcta.
+            <br />
+            <br />
+            La página utiliza una estructura típica de un proyecto Svelte con Sveltekit
+            y está desplegado en Vercel, la ausencia de un backend permite reducir
+            la complejidad y aumentar el rendimiento de la aplicación significativamente,
+            esto supone que el código para la conversión se ejecuta en el navegador,
+            pero este ha sido optimizado para funcionar con archivos de gran tamaño
+            y complejidad.
+            <br />
+            <br />
+            Puedes encontrar más información en:
+            <a target="_blank" href="https://github.com/ign13101/ebraic"
+                >https://github.com/ign13101/ebraic</a
+            >
         </div>
 
         <br />
